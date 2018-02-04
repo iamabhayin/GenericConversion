57d64027b2645f1d7c56c22a0271bba31bb77e5d
iamabhayin <iamabhayin@gmail.com>
2018-02-04 16:20:00 +0530

    Added methods of conversion.

--- GenericConversion/ToGenericList.cs
+++ GenericConversion/ToGenericList.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GenericConversion
 {
-    public class ToGenericList
+    internal class ToGenericList
     {
+        public static List<T> ConvertDataTable<T>(DataTable dt)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in dr.Table.Columns)
+            {
+                foreach (PropertyInfo pro in temp.GetProperties())
+                {
+                    if (pro.Name == column.ColumnName)
+                        pro.SetValue(obj, dr[column.ColumnName], null);
+                    else
+                        continue;
+                }
+            }
+            return obj;
+        }
     }
 }
---
+++ GenericConversion/ToJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace GenericConversion
+{
+    internal class ToJson
+    {
+        public string DataTableToJson(DataTable table)
+        {
+            string JSONString = string.Empty;
+            JSONString = JsonConvert.SerializeObject(table);
+            return JSONString;
+        }
+
+        public string ListToJson<T>(T list)
+        {
+            string JSONString = string.Empty;
+            JSONString = JsonConvert.SerializeObject(list);
+            return JSONString;
+        }
+    }
+}
